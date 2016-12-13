75d4e25758d25265238e48053cc2e4ffeab1aaa8
Frédéric Pirson <frederic.pirson.external@atos.net>
2016-12-13 09:28:25 +0100

    Change mirrors settings

--- integration-testing/container/assets/scripts/install-java.sh
+++ integration-testing/container/assets/scripts/install-java.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Download Java"
+yum clean metadata
+echo "minrate=1\n" >> /etc/yum.conf
+echo "timeout=100\n" >> /etc/yum.conf
+
 yum install -y java-1.7.0-openjdk
 
 #wget --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u5-b13/jdk-8u5-linux-x64.tar.gz
